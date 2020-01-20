f9027fe318e9b046e06be54dc7d279923d7c7a98
mcortes <mykmyk@gmail.com>
2020-01-20 21:02:27 +0800

    update header, footer, sidebar layouts

--- resources/views/home/layouts/footer.blade.php
+++ resources/views/home/layouts/footer.blade.php
@@ -2,10 +2,10 @@
 
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014-2019 <a href="http://philgas.ph">philGas.PH</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.2
+      <b>Version</b> 2.0.0
     </div>
   </footer>
 

2f891aedd34c6aa16e9107c98165ce60d15f11ad
mcortes <mykmyk@gmail.com>
2020-03-21 16:49:45 +0800

    permissions

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         Yajra\DataTables\DataTablesServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
